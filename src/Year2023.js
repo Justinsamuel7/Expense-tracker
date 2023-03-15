73d479a01c398c9eb917d6b4f29990f5b47c8491
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-03-15 22:34:27 +0530

    Initial commit

---
+++ src/Year2023.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+
+export default function Year2023() {
+  return (
+    <div>
+      <h1>2023</h1>
+    </div>
+  );
+}
